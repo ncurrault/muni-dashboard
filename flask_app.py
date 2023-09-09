ba7001e1134f0cd5e5549d12f874c7b4e9b26285
Nicholas Currault <nicholas.curr@gmail.com>
2023-09-09 02:28:48 -0700

    add caching

--- flask_app.py
+++ flask_app.py
@@ -5,12 +5,17 @@ from flask import (
     render_template,
 )
 from get_data import get_result
+from flask_caching import Cache
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
 
+app.config.from_mapping({"CACHE_TYPE": "SimpleCache", "CACHE_DEFAULT_TIMEOUT": 60})
+cache = Cache(app)
+
 
 @app.route("/get_data")
+@cache.cached(timeout=59)
 def get_data():
     response = jsonify(get_result())
     response.headers["Content-Type"] = "application/json"
